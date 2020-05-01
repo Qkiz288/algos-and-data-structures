a20596eb57c0f4025d5d3181afcdf09583f46afd
Kamil <qkiz288@gmail.com>
2020-05-01 08:58:51 +0200

    Solved 'squared array' problem using frequency pattern

--- src/problem_solving_patterns/squared_array.js
+++ src/problem_solving_patterns/squared_array.js
@@ -3,8 +3,9 @@
 // [1,2,3], [4,1,9] - true
 // [1,2,3], [1,9] - false
 // [1,2,1]. [4,4,1] - false
-// Below solution has O(n^2) complexity
-module.exports = function naiveSolution(array1, array2) {
+
+// Below solution is O(n^2) complexity, it has nested loops which should be avoided
+module.exports.naiveSolution = function naiveSolution(array1, array2) {
     // check if arrays have the same size
     if (array1.length !== array2.length) {
         return false;
@@ -24,4 +25,50 @@ module.exports = function naiveSolution(array1, array2) {
         array2.splice(index, 1);
     }
     return true
+}
+
+// Better solution with frequency pattern
+// Below solution is O(n)
+module.exports.betterSolution = function betterSolution(array1, array2) {
+    // check if arrays have the same length
+    if (array1.length !== array2.length) {
+        return false;
+    }
+
+    // create frequency objects for both arrays
+    const frequency1 = {};
+    const frequency2 = {};
+
+    // loop through array1
+    for (const value of array1) {
+        // check if value exists in frequency1
+        // YES -> increment
+        // NO -> initialize to 1
+        frequency1[value] = frequency1[value] ? frequency1[value] + 1 : 1;
+    }
+
+    // loop through array2
+    for (const value of array2) {
+    // check if value exists in frequency2
+        // YES -> increment
+        // NO -> initialize to 1   
+        frequency2[value] = frequency2[value] ? frequency2[value] + 1: 1;
+    }
+
+    // loop through frequency1
+    for (key in frequency1) {
+        
+        // check if squared key from frequency1 exists in frequency2
+        if (!(key ** 2 in frequency2)) {
+            return false;
+        }
+
+        // check if frequency1 key appears the same amount of times as corresponding frequency2 key
+        if (frequency1[key] !== frequency2[key ** 2]) {
+            return false;
+        }
+    }
+
+    // if everything went well, return true
+    return true;
 }
--- src/problem_solving_patterns/test.js
+++ src/problem_solving_patterns/test.js
@@ -1,3 +1,7 @@
-const naiveSolution = require('./squared_array');
+const squared_array = require('./squared_array');
 
-console.log(naiveSolution([2, 4, 3, 2], [9, 4, 16, 4]));
+console.log(squared_array.naiveSolution([2, 4, 3, 2], [9, 4, 16, 4]));
+console.log(squared_array.naiveSolution([2, 4, 3], [9, 4, 16, 4]));
+
+console.log(squared_array.betterSolution([2, 4, 3, 2], [9, 4, 16, 4]));
+console.log(squared_array.betterSolution([2, 4, 3, 2], [16, 4, 16, 4]));

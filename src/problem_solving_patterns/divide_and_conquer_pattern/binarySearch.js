e1d24c6eabb6937601338361b74d488d211b26c8
Kamil <qkiz288@gmail.com>
2020-05-05 10:30:37 +0200

    binarySearch algo using divide and conquer pattern

--- src/problem_solving_patterns/divide_and_conquer_pattern/binarySearch.js
+++ src/problem_solving_patterns/divide_and_conquer_pattern/binarySearch.js
@@ -1,9 +1,9 @@
-// given a sorted array of integers and a number, find the
+// given a SORTED array of integers and a number, find the
 // index where value is located in the array.
 // if not found - return -1
 
 // naive solution, time complexity O(n)
-module.exports = function naiveSolution(array, number) {
+module.exports.naiveSolution = function naiveSolution(array, number) {
     let index = -1;
     // iterate through array
     for (let i = 0; i < array.length; i++) {
@@ -11,6 +11,41 @@ module.exports = function naiveSolution(array, number) {
         if (array[i] === number) {
             index = i;
             break;
+        }
+    }
+    return index;
+}
+
+// better solution using divide and conquer pattern - time compelxity log(n)
+module.exports.betterSolution = function betterSolution(array, number) {
+    // declare min and max variables that hold boundaries of dividing
+    let min = 0;
+    let max = array.length - 1;
+    let index = -1;
+
+    // iterate until min and max overlap
+    while (min <= max) {
+
+        // get the middle index of the array/subarray
+        let middleIndex = Math.floor((min + max) / 2)
+
+        // get the number from that index
+        let currentNumber = array[middleIndex];
+
+        // if searched number is greater than current, 
+        // cut the left boundary of searching (including already checked value)
+        if (currentNumber < number) {
+            min = middleIndex + 1;
+        } 
+        // if searched number is lesser than current, 
+        // cut the right boundary of searching (including already checked value)
+        else if (currentNumber > number) {
+            max = middleIndex - 1;
+        } 
+        // value found - save its index
+        else {
+            index = middleIndex;
+            break; 
         }
     }
     return index;
--- src/problem_solving_patterns/divide_and_conquer_pattern/test.js
+++ src/problem_solving_patterns/divide_and_conquer_pattern/test.js
@@ -1,4 +1,7 @@
 const binarySearch = require('./binarySearch');
 
-console.log("Naive solution: " + binarySearch([1,2,3,7,11,12,15], 12));
-console.log("Naive solution: " + binarySearch([1,2,3,7,11,12,15], 13));
+console.log("Naive solution: " + binarySearch.naiveSolution([1,2,3,7,11,12,15,16], 12));
+console.log("Naive solution: " + binarySearch.naiveSolution([1,2,3,7,11,12,15,16], 13));
+
+console.log("Better solution: " + binarySearch.betterSolution([1,2,3,7,11,12,15,16], 12));
+console.log("Better solution: " + binarySearch.betterSolution([1,2,3,7,11,12,15,16], 13));

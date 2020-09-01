fe14cac2a0be9e0773a40d97d66e0518009ff887
Kamil <qkiz288@gmail.com>
2020-09-01 09:05:07 +0200

    Adding test coverage for MathParser

--- src/algorithms/recursiveDescentParser/test.js
+++ src/algorithms/recursiveDescentParser/test.js
@@ -4,11 +4,23 @@ describe("Recursive Descent Parser tests", function() {
 
     [
         {num1: 1, num2: 2, operator: "+", expectedResult: 3},
+        {num1: 10, num2: 2, operator: "+", expectedResult: 12},
+        {num1: 1, num2: 20, operator: "+", expectedResult: 21},
         {num1: -1, num2: 2, operator: "+", expectedResult: 1},
+        {num1: -10, num2: 2, operator: "+", expectedResult: -8},
+        {num1: -1, num2: 20, operator: "+", expectedResult: 19},
         {num1: 1, num2: 2, operator: "-", expectedResult: -1},
+        {num1: 10, num2: 2, operator: "-", expectedResult: 8},
+        {num1: 1, num2: 20, operator: "-", expectedResult: -19},
         {num1: -1, num2: 2, operator: "-", expectedResult: -3},
+        {num1: -10, num2: 2, operator: "-", expectedResult: -12},
+        {num1: -1, num2: 20, operator: "-", expectedResult: -21},
         {num1: 0, num2: 0, operator: "-", expectedResult: 0},
+        {num1: 00, num2: 0, operator: "-", expectedResult: 0},
+        {num1: 0, num2: 00, operator: "-", expectedResult: 0},
         {num1: 0, num2: 0, operator: "+", expectedResult: 0},
+        {num1: 00, num2: 0, operator: "+", expectedResult: 0},
+        {num1: 0, num2: 00, operator: "+", expectedResult: 0},
     ]
         .forEach(testData => 
             it("Valid two numbers addition/subtraction expression", function() {
@@ -26,15 +38,45 @@ describe("Recursive Descent Parser tests", function() {
     
     [
         {num1: 1, num2: 2, num3: 3, operator1: "+", operator2: "+", expectedResult: 6},
+        {num1: 10, num2: 2, num3: 3, operator1: "+", operator2: "+", expectedResult: 15},
+        {num1: 1, num2: 20, num3: 3, operator1: "+", operator2: "+", expectedResult: 24},
+        {num1: 1, num2: 2, num3: 30, operator1: "+", operator2: "+", expectedResult: 33},
         {num1: -1, num2: 2, num3: 3, operator1: "+", operator2: "+", expectedResult: 4},
+        {num1: -10, num2: 2, num3: 3, operator1: "+", operator2: "+", expectedResult: -5},
+        {num1: -1, num2: 20, num3: 3, operator1: "+", operator2: "+", expectedResult: 22},
+        {num1: -1, num2: 2, num3: 30, operator1: "+", operator2: "+", expectedResult: 31},
         {num1: 1, num2: 2, num3: 3, operator1: "-", operator2: "-", expectedResult: -4},
+        {num1: 10, num2: 2, num3: 3, operator1: "-", operator2: "-", expectedResult: 5},
+        {num1: 1, num2: 20, num3: 3, operator1: "-", operator2: "-", expectedResult: -22},
+        {num1: 1, num2: 2, num3: 30, operator1: "-", operator2: "-", expectedResult: -31},
         {num1: -1, num2: 2, num3: 3, operator1: "-", operator2: "-", expectedResult: -6},
+        {num1: -10, num2: 2, num3: 3, operator1: "-", operator2: "-", expectedResult: -15},
+        {num1: -1, num2: 20, num3: 3, operator1: "-", operator2: "-", expectedResult: -24},
+        {num1: -1, num2: 2, num3: 30, operator1: "-", operator2: "-", expectedResult: -33},
         {num1: 0, num2: 0, num3: 0, operator1: "-", operator2: "-", expectedResult: 0},
+        {num1: 00, num2: 0, num3: 0, operator1: "-", operator2: "-", expectedResult: 0},
+        {num1: 0, num2: 00, num3: 0, operator1: "-", operator2: "-", expectedResult: 0},
+        {num1: 0, num2: 0, num3: 00, operator1: "-", operator2: "-", expectedResult: 0},
         {num1: 0, num2: 0, num3: 0, operator1: "+", operator2: "+", expectedResult: 0},
+        {num1: 00, num2: 0, num3: 0, operator1: "+", operator2: "+", expectedResult: 0},
+        {num1: 0, num2: 00, num3: 0, operator1: "+", operator2: "+", expectedResult: 0},
+        {num1: 0, num2: 0, num3: 00, operator1: "+", operator2: "+", expectedResult: 0},
         {num1: 1, num2: 2, num3: 3, operator1: "+", operator2: "-", expectedResult: 0},
+        {num1: 10, num2: 2, num3: 3, operator1: "+", operator2: "-", expectedResult: 9},
+        {num1: 1, num2: 20, num3: 3, operator1: "+", operator2: "-", expectedResult: 18},
+        {num1: 1, num2: 2, num3: 30, operator1: "+", operator2: "-", expectedResult: -27},
         {num1: -1, num2: 2, num3: 3, operator1: "+", operator2: "-", expectedResult: -2},
+        {num1: -10, num2: 2, num3: 3, operator1: "+", operator2: "-", expectedResult: -11},
+        {num1: -1, num2: 20, num3: 3, operator1: "+", operator2: "-", expectedResult: 16},
+        {num1: -1, num2: 2, num3: 30, operator1: "+", operator2: "-", expectedResult: -29},
         {num1: 1, num2: 2, num3: 3, operator1: "-", operator2: "+", expectedResult: 2},
-        {num1: -1, num2: 2, num3: 3, operator1: "-", operator2: "+", expectedResult: 0}
+        {num1: 10, num2: 2, num3: 3, operator1: "-", operator2: "+", expectedResult: 11},
+        {num1: 1, num2: 20, num3: 3, operator1: "-", operator2: "+", expectedResult: -16},
+        {num1: 1, num2: 2, num3: 30, operator1: "-", operator2: "+", expectedResult: 29},
+        {num1: -1, num2: 2, num3: 3, operator1: "-", operator2: "+", expectedResult: 0},
+        {num1: -10, num2: 2, num3: 3, operator1: "-", operator2: "+", expectedResult: -9},
+        {num1: -1, num2: 20, num3: 3, operator1: "-", operator2: "+", expectedResult: -18},
+        {num1: -1, num2: 2, num3: 30, operator1: "-", operator2: "+", expectedResult: 27},
     ]
         .forEach(testData => 
             it("Valid three numbers addition/subtraction expression", function() {
@@ -85,8 +127,12 @@ describe("Recursive Descent Parser tests", function() {
             );
 
     [
-        {num1: 1, num2: 2, operator: "*", expectedResult: 2},   
-        {num1: 10, num2: 2, operator: "*", expectedResult: 20}    
+        {num1: 1, num2: 2, operator: "*", expectedResult: 2}, 
+        {num1: -1, num2: 2, operator: "*", expectedResult: -2},   
+        {num1: 10, num2: 2, operator: "*", expectedResult: 20}, 
+        {num1: -10, num2: 2, operator: "*", expectedResult: -20},
+        {num1: 1, num2: 20, operator: "*", expectedResult: 20},
+        {num1: -1, num2: 20, operator: "*", expectedResult: -20}, 
     ]
         .forEach(testData => 
             it("Valid two numbers multiplication expression", function() {
